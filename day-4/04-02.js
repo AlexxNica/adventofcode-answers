5a4428c4d9e4dc99f00ba7d7858a909a82fbdc09
Rachel Smith <contact@rachsmith.com>
2015-12-05 14:56:53 -0800

    day 4

---
+++ day-4/04-02.js
@@ -0,0 +1,16 @@
+var input = 'bgvyzdsv';
+var match = false;
+var num = 1;
+
+var md5 = require('md5');
+
+while (!match) {
+    var hex = md5(input+num);
+    if (hex.indexOf('000000') == 0) {
+        match = true;
+    } else {
+        num++;
+    }
+}
+
+console.log(num);
